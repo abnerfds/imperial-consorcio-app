0442f532c04f092f754f8d9588aafdf7acdf112b
Abner <abnerferreira84@gmail.com>
2023-10-13 11:52:08 -0300

    [fix]: solução bug do background da home 2.

--- src/app/components/Advantage/Advantage.tsx
+++ src/app/components/Advantage/Advantage.tsx
@@ -9,7 +9,7 @@ import money from "../../../assets/images/money.svg";
 
 const Advantage = () => {
   return (
-    <section id="advantages" className="h-full flex flex-col justify-center py-40">
+    <section id="advantages" className="flex flex-col justify-center">
       <section className="text-center mb-20">
         <h2 className="text-[#42B4EE] sm:text-5xl text-4xl font-bold mb-5">As vantagens de escolher o consórcio Imperial</h2>
         <h3 className="text-[#6b7280] text-2xl">Isso e muito mais.</h3>
