3ef994107f8a94c9b77191b262d8168f36b6c496
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2023-10-13 11:32:34 -0300

    [fix]: removendo importação inexistente.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Advantage from "./components/Advantage/Advantage";
 import InitialPage from "./components/InitialPage/InitialPage";
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import StepsHowItWorks from "./components/StepsHowItWorks/StepsHowItWorks";
 import WhatIs from "./components/WhatIs/WhatIs";
 
