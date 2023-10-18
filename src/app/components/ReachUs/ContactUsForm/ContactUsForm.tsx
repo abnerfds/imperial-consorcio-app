64e774cf799f6ac0e9217dedbee14d1c22e1e23d
Abner <abnerferreira84@gmail.com>
2023-10-18 19:49:58 -0300

    feat: ajustando responsividade da tela ReachUs.

--- src/app/components/ReachUs/ContactUsForm/ContactUsForm.tsx
+++ src/app/components/ReachUs/ContactUsForm/ContactUsForm.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const ContactUsForm = () => {
   return (
-    <form action="" className='text-black rounded-lg flex flex-col gap-5 pb-24'>
-        <h1 className='text-white text-2xl text-center max-w-xs font-bold mb-4'>Preencha seus dados e entraremos em contato</h1>
-        <input type="text" placeholder="Nome e Sobrenome" className="input max-w-xs bg-white w-full" />
-        <input type="text" placeholder="E-mail" className="input max-w-xs bg-white w-full" />
-        <input type="text" placeholder="Telefone" className="input max-w-xs bg-white w-full" />
-        <button className='btn bg-btn-reverse max-w-xs border-none text-lg text-white'>Enviar</button>
+    <form action="" className='text-black rounded-lg flex flex-col gap-5 pb-24 md:pb-0'>
+        <h1 className='text-white text-2xl text-center max-w-md lg:max-w-sm font-bold mb-4'>Preencha seus dados e entraremos em contato</h1>
+        <input type="text" placeholder="Nome e Sobrenome" className="input max-w-md lg:max-w-sm bg-white w-full" />
+        <input type="text" placeholder="E-mail" className="input max-w-md lg:max-w-sm bg-white w-full" />
+        <input type="text" placeholder="Telefone" className="input max-w-md lg:max-w-sm bg-white w-full" />
+        <button className='btn bg-btn-reverse max-w-md lg:max-w-sm border-none text-lg text-white'>Enviar</button>
     </form>
   )
 }
--- src/app/components/ReachUs/ReachUs.tsx
+++ src/app/components/ReachUs/ReachUs.tsx
@@ -4,13 +4,12 @@ import OtherInfo from "./OtherInfo/OtherInfo"
 
 const ReachUs = () => {
   return (
-    <section className='bg-teste-p w-full min-h-screen py-20 flex flex-col justify-around items-center text-white mb-52'>
+    <section className='bg-teste-p w-full py-20 flex flex-col justify-around items-center text-white mb-52'>
      
       <section className="w-[95%] relative">
         <h3 className={`text-white text-center sm:text-5xl text-4xl font-bold mb-8 lg:mb-16`}>
           Onde nos alcançar
         </h3>
-        {/* min-h-[120vh] lg:min-h-full */}
         <section className='w-full flex flex-col lg:flex-row justify-around lg:justify-center items-center lg:items-start md:gap-12 lg:gap-24 xl:gap-48'>
           <ContactUsForm />
           <OtherInfo />
